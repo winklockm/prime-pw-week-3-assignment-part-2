6d7dc37beb7be54336ea2769138dcd8555866428
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-26 09:39:41 -0500

    console logged numbers 0 to 5

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,6 +13,9 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for (let i=0; i<6; i++) {
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
