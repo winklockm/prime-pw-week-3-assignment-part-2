52d0068946e530fb3092d659e40e70738f2f382a
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-26 09:42:33 -0500

    wrote for look to console.log numbers from 3 to 5

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -20,6 +20,9 @@ for (let i=0; i<6; i++) {
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for(let i=3; i<6; i++) {
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
